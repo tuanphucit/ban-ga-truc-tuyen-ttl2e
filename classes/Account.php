b1bbb54e3fe997b3354eb0639cd059fd33cd533f
vuviettoan@gmail.com <vuviettoan@gmail.com@cbb5bf61-c089-5783-f77a-20339e7f4214>
2010-12-29 19:43:56 +0000

    getInfo & transfer

--- classes/Account.php
+++ classes/Account.php
@@ -2,22 +2,45 @@
 include_once dirname ( __FILE__ ) . "../config/include.inc.php";
 
 class Account {
-	
-	public function __construct($customerID = 0, $accountID = 0) {
+	public $id_account, $id_customer, $balance, $account_number;
+	public function __construct($customerID,$accountID) {
 		/*TODO
 		 * Khoi tao 1 Account , neu 1 trong 2 gia tri = 0
 		 * thi khoi tao theo gia tri khac co gia tri khac 0
 		 */
+		
+		$db = new Db();
+		$db->connect();
 		if ($customerID != 0){
-			//
+			$sql = "select * from account where id_customer = '$customerID'";
+			$result = $db->runQuery($sql);
+			while($row = mysql_fetch_array($result,MYSQL_ASSOC )){
+				$this->balance = $row[balance];
+				$this->account_number = $row[account_number];
+				$this->id_customer = $row[customerID];
+				$this->id_account = $row[account];
+			}
 			return;
 		}
+		if($accountID !=0){
+			$sql = "select * from account where id_account = '$accountID'";
+			$result = $db->runQuery($sql);
+			while($row = mysql_fetch_array($result,MYSQL_ASSOC )){
+				$this->balance = $row[balance];
+				$this->account_number = $row[account_number];
+				$this->id_customer = $row[customerID];
+				$this->id_account = $row[account];
+			}
+			}
+		
 		//
 		return;
+		$db->close();
 	}
-	public $id_account, $id_customer, $balance, $account_number;
+	
 	
 	public function addAcount($_id_customer, $_balance, $_account_number) {
+		// Khi có khác hàng dang kí t?o tài kho?n m?i thì admin ngân hàng s? set 
 		$db = new DB ();
 		$this->id_customer = $_id_customer;
 		$this->balance = $_balance;
@@ -27,6 +50,14 @@ class Account {
 		$db->runQuery ( $sql );
 		$db->close ();
 	}
+	public function updateBalance($id_account,$newBalance){
+		$db= new DB();
+		$db->connect();
+		$sql = "update account set balance = '$newBalance' where id_account = '$id_account'";
+		$db->runQuery($sql);
+		
+		$db->close();
+	}
 	public static function checkAccount($bankAccount) {
 		$db = new DB ();
 		$check = false;
@@ -35,10 +66,20 @@ class Account {
 		if (mysql_num_rows ( $query ) != "") {
 			$check = true;
 		}
+		
 		return $check;
+		$db->close();
 	}
 	
 	public function transfer($accountID, $amount){
+		
+		$acc1 = new Account(0, $accountID);
+		$acc1->balance -= $amount;
+		if($acc1->updateBalance($acc1->id_account, $acc1->balance))
+		{
+			$this->balance += $amount;
+			$this->updateBalance($this->id_account, $this->balance);
+		}
 		
 	}
 }
