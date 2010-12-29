41529c3bad567f2b83a7df510d5545ee82c3e859
luckymancvp <luckymancvp@cbb5bf61-c089-5783-f77a-20339e7f4214>
2010-12-29 17:41:51 +0000

    Thêm cái Account.php

--- trunk/config/include.inc.php
+++ trunk/config/include.inc.php
@@ -2,6 +2,7 @@
 
 include_once dirname(__FILE__)."../../classes/DB.php";
 include_once dirname(__FILE__)."../../classes/Order.php";
+include_once dirname(__FILE__)."../../classes/Account.php";
 include_once dirname(__FILE__)."../../classes/Customer.php";
 include_once dirname(__FILE__)."../../classes/RSA/RsaLib.php";
 
