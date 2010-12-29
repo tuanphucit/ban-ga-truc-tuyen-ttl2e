155389492564218a7dd4507750ce1ee2c8c1f079
vuviettoan@gmail.com <vuviettoan@gmail.com@cbb5bf61-c089-5783-f77a-20339e7f4214>
2010-12-29 18:38:49 +0000

    check account_number

--- classes/Account.php
+++ classes/Account.php
@@ -1,12 +1,31 @@
 <?php
+include_once dirname(__FILE__)."../config/include.inc.php";
 
 class Account{
 	
 	public function __construct(){
 		
 	}
-	
+  public $id_account, $id_customer, $balance, $account_number;
+  
+  public function addAcount($_id_customer,$_balance,$_account_number){
+  	$db = new DB();
+  	$this->id_customer =$_id_customer;
+  	$this->balance =$_balance;
+  	$this->account_number = $_account_number;
+  	$db->connect();
+  	$sql = "insert into account(id_customer,balance,account_number) values ('$_id_customer','$_balance','$_account_number')";
+  	$db->runQuery($sql);
+  	$db->close();
+  }
 	public static function checkAccount($bankAccount){
-		return true;
+		$db = new DB();
+		$check = false;
+		$sql = "select * from account where account_number = '$bankAccount'";
+		$query = $db->runQuery($sql);
+		if(mysql_num_rows($query)!= ""){
+			$check = true;
+		}
+		 return $check;
 	}
 }
